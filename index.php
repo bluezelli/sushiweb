c1c88f4157197fb1a8fc9e5741346ef995cb0296
bluezelli <96504071+bluezelli@users.noreply.github.com>
2022-09-14 16:54:16 +0200

    update

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">    
+<title>GoldenFish</title>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg bg-dark text-light">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">GoldenFish</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Bestellingen</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Pricing</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div
+
+        class="p-5 text-center bg-image"
+
+        style="background-repeat:none;
+        background-size:cover;
+
+          background-image: url('./img/sush.webp');
+
+          height: 200px;
+
+        "
+
+      >
+
+        <div class="mask">
+
+          <div class="d-flex justify-content-center align-items-center h-100">
+
+            <div class="text-white">
+
+              <h1 class="mb-3">Home</h1>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <div class="container">
+        <h1> </h1>
+      </div>
+
+
+
+
+
+
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
